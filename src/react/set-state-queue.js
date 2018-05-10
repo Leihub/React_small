ab5f67d50dcbdad52efd7331e2c5d83aba84dab3
leiyuanyuan <leiyuanyuan@variflight.com>
2018-05-10 10:56:35 +0800

    capter4

--- src/react/set-state-queue.js
+++ src/react/set-state-queue.js
@@ -17,8 +17,32 @@ export function enqueueSetState(statechange,component){
         statechange,
         component
     })
+
+    if(!renderQueue.some(item => item === component)){
+        renderQueue.push(component)
+    }
 }
 
 function flush(){
-    
+    //清空队列
+    let item ,component
+
+    while (item == setStateQueue.shift()) {
+        const {component,statechange} = item
+        if(!component.preState){
+            component.preState = Object.assign({},statechange)
+        }
+
+        if(typeof statechange === 'function'){
+            Object.assign(component.state,statechange(component.preState,component.props))
+        }else{
+            Object.assign(component.state,statechange)
+        }
+
+        component.preState = component.state
+    }
+
+    while(component == renderQueue.shift()){
+        renderComponent(component)
+    }
 }
